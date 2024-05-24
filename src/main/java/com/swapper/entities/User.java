c4f1dba8cf2a0542e2d92f5b6501c4d848185f19
Diego Andre Poli <diegoandrepoli@gmail.com>
2024-05-24 12:07:19 -0400

    Add user entity

---
+++ src/main/java/com/swapper/entities/User.java
@@ -0,0 +1,76 @@
+package com.swapper.entities;
+
+import java.sql.Timestamp;
+
+public class User {
+
+    private Long id;
+
+    private String email;
+
+    private String name;
+
+    private String password;
+
+    private boolean enabled;
+
+    private Timestamp created;
+
+    private Timestamp updated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Timestamp updated) {
+        this.updated = updated;
+    }
+}
